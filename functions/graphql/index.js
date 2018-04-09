5094735ac28c9620b3c6bff6680c1f2e897f81fb
Andrew Joslin <andrewtjoslin@gmail.com>
2018-04-09 10:08:02 -0600

    Send down jwt in `jcrew_jwt` cookie.

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -1,5 +1,6 @@
 var server = require('apollo-server-lambda')
 var jwtVerify = require('../../controllers/auth/jwt-verify')
+var Url = require('url')
 var schema = require('../../graphql')
 
 exports.graphiqlHandler = server.graphiqlLambda({
@@ -24,16 +25,21 @@ exports.graphqlHandler = (event, context, callback) => {
 
   function handleGraphqlResponse (error, data) {
     if (error) return callback(error)
+    const origin = event.headers.origin || event.headers.Origin || ''
 
     // Add CORS headers
     // see: https://serverless.com/blog/cors-api-gateway-survival-guide/#cors-with-cookie-credentials
     if (data.headers) {
-      data.headers['Access-Control-Allow-Origin'] = event.headers.origin
+      data.headers['Access-Control-Allow-Origin'] = origin
       data.headers['Access-Control-Allow-Credentials'] = true
       data.headers['Access-Control-Expose-Headers'] = 'Authorization'
     }
     if (appContext.jwt) {
       data.headers.Authorization = `Bearer ${appContext.jwt}`
+      data.headers['Set-Cookie'] = data.headers['Set-Cookie'] || ''
+      data.headers['Set-Cookie'] += ` jcrew_jwt=${appContext.jwt}; domain=${
+        Url.parse(origin).hostname
+      };`
     }
 
     callback(null, data)
